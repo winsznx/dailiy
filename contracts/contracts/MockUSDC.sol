b69e74545cc17a5395be637ca8d789d4f92e5f72
winsznx <timjosh507@gmail.com>
2025-12-20 18:07:35 +0100

    feat: add MockUSDC for testing

---
+++ contracts/contracts/MockUSDC.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockUSDC is ERC20 {
+    constructor() ERC20("USD Coin", "USDC") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+    
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
